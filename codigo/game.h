ea945a415bd5289121edac295560d74bfdd8af48
Pedro Marinho <pmpc@cesar.school>
2025-11-13 22:12:01 -0300

    cabeçalho game.h com struct GameState básica

---
+++ codigo/game.h
@@ -0,0 +1,35 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <stdbool.h>
+#include "../cli-lib/include/screen.h"
+#include "../cli-lib/include/keyboard.h"
+#include "../cli-lib/include/timer.h"
+
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 24
+
+typedef enum {
+    MENU,
+    PLAYING,
+    GAME_OVER
+} GameStatus;
+
+typedef struct {
+    int raquete_esquerda, raquete_direita;
+    int bola_x, bola_y;
+    int bola_dir_x, bola_dir_y;
+    int placar_esquerda, placar_direita;
+    bool quit;
+    GameStatus status;
+    int jogador_vencedor;
+    char **campo;
+} GameState;
+
+void jogo_inicio(GameState *game);
+void processar_input(GameState *game);
+void atualizar_jogo(GameState *game);
+void renderizar(GameState *game);
+void liberar(GameState *game);
+
+#endif
